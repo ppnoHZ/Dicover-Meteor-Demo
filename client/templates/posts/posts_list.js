fffc89c20cfc30e38a0e5d877d2580798f2ce48e
zhoudd <zdd_910@126.com>
2015-07-30 08:55:30 +0800

    完毕。

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -10,18 +10,18 @@ Template.postsList.rendered = function () {
     this.find('.wrapper')._uihooks = {
         moveElement: function (node, next) {
             var $node = $(node), $next = $(next);
-            var oldTop = $node.offset().top;
-            var height = $node.outerHeight(true);
+            var oldTop = $(node).offset().top;
+            var height = $(node).outerHeight(true);
 
             //取两个元素之间的元素
-            var $inBetween = $next.nextUntil(node);
+            var $inBetween = $(next).nextUntil(node);
             if ($inBetween.length === 0) {
-                $inBetween = $node.nextUntil(next);
+                $inBetween = $(node).nextUntil(next);
             }
 
-            $node.insertBefore(next);
+            $(node).insertBefore(next);
 
-            var newTop = $node.offset().top;
+            var newTop = $(node).offset().top;
 
             $node
                 .removeClass('animate')
@@ -30,8 +30,8 @@ Template.postsList.rendered = function () {
             $inBetween
                 .removeClass('animate')
                 .css('top', oldTop < newTop ? height : -1 * height);
-            $node.offset();
-            $node.addClass('animate').css('top', 0);
+            $(node).offset();
+            $(node).addClass('animate').css('top', 0);
             $inBetween.addClass('animate').css('top', 0);
         },
         insertElement: function (node, next) {
